f7c94a7403fb9be472d948be2750eeb91a499dbe
shrikrishnadubey <kisan.dubey7@gmail.com>
2018-12-28 22:02:22 +0530

    ch05

--- Ch05/ProgrammingProjects/p03.c
+++ Ch05/ProgrammingProjects/p03.c
@@ -1,4 +1,15 @@
-/* Calculates a broker's commission */
+/* Modify the broker.c program of Section 5.2 by making both of the following
+ * changes:
+ *
+ * (a) Ask the user to enter the number of shares and the price per share,
+ * instead of the value of the trade.
+ * (b) Add statements that compute the commission charged by a rival broker
+ * ($33 plus 3 cents per share for fewer than 2000 shares; $33 plus 2 cents per
+ * share for 2000 shares or more). Display the rival's commission as well as
+ * the commission charged by the original broker.
+ */
+
+/* Not sure what to do with this one, so I'll call it incomplete.
 
 #include <stdio.h>
 
@@ -29,3 +40,4 @@ int main(void)
 
     return 0;
 }
+*/
